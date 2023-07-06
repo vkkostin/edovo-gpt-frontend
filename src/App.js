1defd9f4c3e2d2a0e638f5c99f660ae5fac0e356
vladickostin <vladic.kostin@edovo.com>
2023-07-06 00:33:22 -0500

    Add stuff

--- src/App.js
+++ src/App.js
@@ -20,77 +20,118 @@ const uploadToServer = (file, onUploadProgress) => {
   };
 
 function App() {
-    const [selectedFiles, setSelectedFiles] = useState(undefined);
-    const [currentFile, setCurrentFile] = useState(undefined);
-    const [message, setMessage] = useState("");
-    const [progress, setProgress] = useState(0);
-    const [data, setData] = useState([]);
-    const [prompt, setPrompt] = useState('');
+  const [selectedFiles, setSelectedFiles] = useState(undefined);
+  const [currentFile, setCurrentFile] = useState(undefined);
+  const [message, setMessage] = useState("");
+  const [progress, setProgress] = useState(0);
+  const [data, setData] = useState([]);
+  const [prompt, setPrompt] = useState('');
 
-    const upload = () => {
-        let currentFile = selectedFiles[0];
-    
-        setProgress(0);
-        setCurrentFile(currentFile);
-    
-        uploadToServer(currentFile, (event) => {
-          setProgress(Math.round((100 * event.loaded) / event.total));
+  const upload = () => {
+      let currentFile = selectedFiles[0];
+  
+      setProgress(0);
+      setCurrentFile(currentFile);
+  
+      uploadToServer(currentFile, (event) => {
+        setProgress(Math.round((100 * event.loaded) / event.total));
+      })
+        .then(async (response) => {
+          setMessage(response.data.message);
         })
-          .then(async (response) => {
-            setMessage(response.data.message);
-          })
-          .catch(() => {
-            setProgress(0);
-            setMessage("Could not upload the file!");
-            setCurrentFile(undefined);
-          });
-    
-        setSelectedFiles(undefined);
-      };
-
-    const selectFile = (event) => {
-        setSelectedFiles(event.target.files);
+        .catch(() => {
+          setProgress(0);
+          setMessage("Could not upload the file!");
+          setCurrentFile(undefined);
+        });
+  
+      setSelectedFiles(undefined);
     };
 
-    const submit = () => {
-        return axios.post(`${URL}/submit`, {prompt});
-    }
+  const selectFile = (event) => {
+      setSelectedFiles(event.target.files);
+  };
 
-    const changePrompt = event => {
-        setPrompt(event.target.value);
+  const submit = async () => {
+    const {data: {currentItem, totalItems}} = await axios.get(`${URL}/progress`);
+
+    const current = parseInt(currentItem, 10);
+    const total = parseInt(totalItems, 10);
+
+    if (current || total) {
+      window.alert(`Processing ${currentItem} / ${totalItems} items. Please wait before trying another prompt.`);
+    } else {
+      if (window.confirm('Submitting your prompt will submit your data to OpenAI. You will not be able to submit another batch of data until the current batch of data has processed. Are you sure you want to continue?')) {
+        return await axios.post(`${URL}/submit`, {prompt});
+      }
     }
+  }
+
+  const changePrompt = event => {
+      setPrompt(event.target.value);
+  }
+
+  const checkProgress = async () => {
+    const {data: {currentItem, totalItems}} = await axios.get(`${URL}/progress`);
+
+    const current = parseInt(currentItem, 10);
+    const total = parseInt(totalItems, 10);
+
+    if (current || total) {
+      window.alert(`Processing ${currentItem} / ${totalItems} items`);
+    } else {
+      window.alert(`Not Processing Any Data`);
+    }
+  }
 
   return (
     <div className="App">
-      <div>
-        <label className="btn btn-default">
-            <input type="file" onChange={selectFile} />
-        </label>
+      <div className="section section--upload">
+        <h3>DATA UPLOAD:</h3>
+        <p>
+          Your CSV <strong>MUST</strong> have "answer" and "question" headers.
+        </p>
+        <div>
+          <label className="btn btn-default">
+              <input type="file" onChange={selectFile} accept=".csv"/>
+          </label>
 
+          <button
+              className="btn btn-success"
+              disabled={!selectedFiles}
+              onClick={upload}
+          >
+              Upload CSV
+          </button>
+
+          <div>
+              Progress: {progress}%
+          </div>
+        </div>
+      </div>
+
+      <div className="section section--prompt">
+        <h3>PROMPT:</h3>
+        <p>
+          The prompt supports two dynamic data tokens: <strong>&#123;&#123;answer&#125;&#125;</strong> and <strong>&#123;&#123;question&#125;&#125;</strong>. These tokens will be replaced by the question text and the learners actual answer before being submitted to OpenAI.
+        </p>
+        <textarea className="prompt-input" onChange={changePrompt}/>
         <button
-            className="btn btn-success"
-            disabled={!selectedFiles}
-            onClick={upload}
-        >
-            Upload
-        </button>
+            onClick={submit}
+            disabled={!prompt}
+        >Submit to AI</button>
+      </div>
 
-        <div>
-            {progress}%
-        </div>
+      <div className="section section--prompt">
+        <button
+            className="progress"
+            onClick={checkProgress}
+        >CHECK PROGRESS</button>
+      </div>
 
-        <div>
-            <input onChange={changePrompt}/>
-            <button
-                onClick={submit}
-                disabled={!prompt}
-            >Submit to AI</button>
-        </div>
-
-        <div>
-            <a href={`${URL}/download`} target="_blank">Download</a>
-        </div>
-    </div>
+      <div>
+          <a href={`${URL}/download`} target="_blank">Download</a>
+      </div>
     </div>
   );
 }
